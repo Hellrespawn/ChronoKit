dee9aa29e3ea5ec9e43017ce7c4c6707a0f16d5f
Stef Korporaal <stef@skorporaal.com>
2026-06-27 14:14:57 +0200

    Use stopwatch icon for shortcut bar button

--- data.lua
+++ data.lua
@@ -40,24 +40,14 @@ data:extend(
 			flags        = { "gui-icon" },
 		},
 		{
-			type       = "shortcut",
-			name       = "chronokit-toggle",
-			action     = "lua",
-			toggleable = true,
-			icons = {
-				{
-					icon      = "__ChronoKit__/graphics/play.png",
-					icon_size = 32,
-					tint      = { r = 0.066, g = 0.066, b = 0.066, a = 1 },
-				},
-			},
-			small_icons = {
-				{
-					icon      = "__ChronoKit__/graphics/play.png",
-					icon_size = 32,
-					tint      = { r = 0.066, g = 0.066, b = 0.066, a = 1 },
-				},
-			},
+			type            = "shortcut",
+			name            = "chronokit-toggle",
+			action          = "lua",
+			toggleable      = true,
+			icon            = "__ChronoKit__/graphics/clock.png",
+			icon_size       = 32,
+			small_icon      = "__ChronoKit__/graphics/clock.png",
+			small_icon_size = 32,
 		},
 	}
 )
